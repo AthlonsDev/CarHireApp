30d513f443dbd062774fe4c90cd9d1b9db129619
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2023-02-08 11:10:53 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        main.mainMenu();
+        main.selectOption(scanner);
+
+    }
+
+    private void mainMenu() {
+
+        System.out.println("****************Welcome To Car Hire System****************");
+        System.out.println("1. Register As A Customer");
+        System.out.println("2. See List Of Cars");
+        System.out.println("3. Search For a Car");
+
+        
+    }
+
+    private void selectOption(Scanner scanner) {
+        
+        switch (scanner.nextInt()) {
+
+            case 1:
+                System.out.println("Register As A Customer");
+                break;
+            case 2:
+                System.out.println("See List Of Cars");
+                break;
+            case 3:
+                System.out.println("Search For a Car");
+                break;
+            default:
+                System.out.println("Invalid Selection");
+                break;
+        }
+    }
+
+}
